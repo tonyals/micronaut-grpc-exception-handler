7d79cc805cb815893a596c44c8b83a7d294b908b
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2021-10-20 19:40:19 -0300

    refactor: create separated class GrpcExceptionHandlerInterceptor

--- micronaut-grpc-exception-handler/src/main/java/br/com/tony/interceptor/GrpcExceptionHandlerInterceptor.java
+++ micronaut-grpc-exception-handler/src/main/java/br/com/tony/interceptor/GrpcExceptionHandlerInterceptor.java
@@ -1,6 +1,6 @@
 package br.com.tony.interceptor;
 
-import br.com.tony.exception.BaseBusinessException;
+import br.com.tony.listener.ExceptionHandlingServerCallListener;
 import io.grpc.*;
 import jakarta.inject.Singleton;
 
@@ -11,43 +11,5 @@ public class GrpcExceptionHandlerInterceptor implements ServerInterceptor {
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
         ServerCall.Listener<ReqT> listener = next.startCall(call, headers);
         return new ExceptionHandlingServerCallListener<>(listener, call, headers);
-    }
-
-    private static class ExceptionHandlingServerCallListener<ReqT, RespT>
-            extends ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT> {
-
-        private final ServerCall<ReqT, RespT> serverCall;
-        private final Metadata metadata;
-
-        public ExceptionHandlingServerCallListener(ServerCall.Listener<ReqT> delegate, ServerCall<ReqT, RespT> serverCall, Metadata metadata) {
-            super(delegate);
-            this.serverCall = serverCall;
-            this.metadata = metadata;
-        }
-
-        @Override
-        public void onHalfClose() {
-            try {
-                super.onHalfClose();
-            } catch (BaseBusinessException e) {
-                serverCall.close(e.statusCode().toStatus().withDescription(e.errorMessage()), metadata);
-                throw e;
-            }
-        }
-
-        @Override
-        public void onCancel() {
-            super.onCancel();
-        }
-
-        @Override
-        public void onComplete() {
-            super.onComplete();
-        }
-
-        @Override
-        public void onReady() {
-            super.onReady();
-        }
     }
 }
