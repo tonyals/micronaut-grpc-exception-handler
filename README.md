5c82287dbd4c21cd8b38578bbb0b1c96e925934d
Tony A. Luz <43297863+TonyALS@users.noreply.github.com>
2021-10-20 20:55:39 -0300

    Develop -> Main (#6)
    * feat: create initial interceptor
    
    * feat: create initial exceptions and basic interceptor
    
    * refactor: change project structure
    
    * feat: configure build.gradle to generate jar
    
    * refactor: change group and version
    
    * doc: add readme
    
    * refactor: change project structure and add exception-handler-demo
    
    * refactor: jar lib path
    
    * Feat/exception handler listener improvements (#5)
    
    * refactor: create separated class GrpcExceptionHandlerInterceptor
    
    * feat: add logger enum
    
    * feat: add demo improvements
    
    * feat: add .gitignore

--- README.md
+++ README.md
@@ -4,13 +4,18 @@ Este repositório possui uma implementação inicial de um ServerInterceptor do 
 geradas a partir de uma rota gRPC. A princípio somente exceções que estendem de BaseBusinessException estão
 sendo capturadas.
 
-A hierarquia atual das exceções está definida desta forma:
+A estrutura atual de exceções está da seguinte forma:
 
 <img src="./img/initial-structure.png" alt="Estrutura inicial"/>
+
+Para você implementar uma exceção customizada que seja capturada pelo interceptor basta estender da **BaseBusinessException** e adicionar sua própria mensagem e código de erro.
+
+## Testando a lib localmente
 
 Para testar a implementação basta executar a task `shadowJar` do projeto `micronaut-grpc-exception-handler` e
 importar o jar gerado no build.gradle do projeto `exception-handler-demo` substituindo o caminho 
 `implementation files("diretorio-do-jar/micronaut-grpc-exception-handler-0.0.1.jar")` pelo path onde foi salvo o jar gerado anteriormente.
+Após importar o jar, ainda no `exception-handler-demo`, abra o pacote controller e dentro da classe **ClientController** há uma lista com alguns nomes de exemplo, para obter uma exceção basta fazer uma request utilizando um nome existente. 
 
 
 ### Referências
--- micronaut-grpc-exception-handler/src/main/java/br/com/tony/interceptor/GrpcExceptionHandlerInterceptor.java
+++ micronaut-grpc-exception-handler/src/main/java/br/com/tony/interceptor/GrpcExceptionHandlerInterceptor.java
@@ -1,53 +1,22 @@
 package br.com.tony.interceptor;
 
-import br.com.tony.exception.BaseBusinessException;
+import br.com.tony.listener.ExceptionHandlingServerCallListener;
 import io.grpc.*;
 import jakarta.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static br.com.tony.constants.LoggerConstants.REQUEST_RECEIVED;
 
 @Singleton
 public class GrpcExceptionHandlerInterceptor implements ServerInterceptor {
 
+    private static final Logger logger = LoggerFactory.getLogger(GrpcExceptionHandlerInterceptor.class);
+
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+        logger.info(REQUEST_RECEIVED.getDescription());
         ServerCall.Listener<ReqT> listener = next.startCall(call, headers);
         return new ExceptionHandlingServerCallListener<>(listener, call, headers);
-    }
-
-    private static class ExceptionHandlingServerCallListener<ReqT, RespT>
-            extends ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT> {
-
-        private final ServerCall<ReqT, RespT> serverCall;
-        private final Metadata metadata;
-
-        public ExceptionHandlingServerCallListener(ServerCall.Listener<ReqT> delegate, ServerCall<ReqT, RespT> serverCall, Metadata metadata) {
-            super(delegate);
-            this.serverCall = serverCall;
-            this.metadata = metadata;
-        }
-
-        @Override
-        public void onHalfClose() {
-            try {
-                super.onHalfClose();
-            } catch (BaseBusinessException e) {
-                serverCall.close(e.statusCode().toStatus().withDescription(e.errorMessage()), metadata);
-                throw e;
-            }
-        }
-
-        @Override
-        public void onCancel() {
-            super.onCancel();
-        }
-
-        @Override
-        public void onComplete() {
-            super.onComplete();
-        }
-
-        @Override
-        public void onReady() {
-            super.onReady();
-        }
     }
 }
